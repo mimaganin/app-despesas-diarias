62d102e4c941a2cd6c9a833dfcc854db3a3d5aef
Milena Maganin <81655437+mimaganin@users.noreply.github.com>
2024-10-12 14:42:41 +0100

    update hosting firebase config

--- dataconnect-generated/js/default-connector/esm/index.esm.js
+++
@@ -1,8 +0,0 @@
-import { getDataConnect, validateArgs } from 'firebase/data-connect';
-
-export const connectorConfig = {
-  connector: 'default',
-  service: 'app-despesas-diarias',
-  location: 'us-central1'
-};
-
